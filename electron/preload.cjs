572db7459659ceff54573e7557eb4d2b4e09a06b
Artemas <arden@lanth.me>
2025-11-28 16:08:10 +0100

    settings

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -2,5 +2,7 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
     setActivity: (activity) => ipcRenderer.send('RPC_SET_ACTIVITY', activity),
-    clearActivity: () => ipcRenderer.send('RPC_CLEAR_ACTIVITY')
+    clearActivity: () => ipcRenderer.send('RPC_CLEAR_ACTIVITY'),
+    enableRPC: () => ipcRenderer.send('RPC_ENABLE'),
+    disableRPC: () => ipcRenderer.send('RPC_DISABLE')
 });
